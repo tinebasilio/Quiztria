7bebdee9e5892b1bb5d30af0b66cd69dcc86b7df
tinebasilio <kristinebasilio28@gmail.com>
2024-10-26 19:39:09 +0800

    Update formatting and comments

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -22,22 +22,23 @@ switch ($width) {
 @endphp
 
 <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+    <!-- Trigger Slot -->
     <div @click="open = ! open">
         {{ $trigger }}
     </div>
 
+    <!-- Content Slot -->
     <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+         x-transition:enter="transition ease-out duration-200"
+         x-transition:enter-start="transform opacity-0 scale-95"
+         x-transition:enter-end="transform opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="transform opacity-100 scale-100"
+         x-transition:leave-end="transform opacity-0 scale-95"
+         class="absolute z-50 mt-2 w-48 rounded-md shadow-lg origin-top-right right-0">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white">
             {{ $content }}
         </div>
     </div>
 </div>
+
