fceb895dab351dcdfe081c1ad32209a1cded114b
tinebasilio <kristinebasilio28@gmail.com>
2024-10-26 21:47:15 +0800

    Add LivewireServiceProvider

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Livewire\LivewireServiceProvider::class,
+
     ])->toArray(),
 
     /*
