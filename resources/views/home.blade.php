44d2b8348befaf09b16798394b4c5a1606d7eccf
tinebasilio <kristinebasilio28@gmail.com>
2024-10-26 19:36:33 +0800

    Add login buttons for admin and participants

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <h6 class="text-xl font-bold">Select how you would like to proceed:</h6>
+
+                    <div class="d-flex justify-content-around mt-4">
+                        <a href="{{ route('login') }}"
+                           class="inline-block bg-blue-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-blue-600">
+                            Admin/User Login
+                        </a>
+                        <a href="{{ route('participant.login') }}"
+                           class="inline-block bg-green-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-green-600">
+                            Participant Login
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
                     <h6 class="text-xl font-bold">Public quizzes</h6>
 
                     @forelse($public_quizzes as $quiz)
@@ -10,8 +31,11 @@
                             <div
                                 class="flex relative flex-col mb-6 min-w-0 break-words bg-white rounded shadow-lg xl:mb-0">
                                 <div class="flex-auto p-4">
-                                    <a href="{{ route('quiz.show', $quiz->slug) }}">{{ $quiz->title }}</a>
-                                    <p class="text-sm">Questions: <span>{{ $quiz->questions_count }}</span></p>
+                                    <a href="{{ route('quiz.show', $quiz->slug) }}"
+                                       class="inline-block bg-blue-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-blue-600">
+                                        {{ $quiz->title }}
+                                    </a>
+                                    <p class="text-sm mt-2">Questions: <span>{{ $quiz->questions_count }}</span></p>
                                 </div>
                             </div>
                         </div>
@@ -34,8 +58,11 @@
                             <div
                                 class="flex relative flex-col mb-6 min-w-0 break-words bg-white rounded shadow-lg xl:mb-0">
                                 <div class="flex-auto p-4">
-                                    <a href="{{ route('quiz.show', $quiz->slug) }}">{{ $quiz->title }}</a>
-                                    <p class="text-sm">Questions: <span>{{ $quiz->questions_count }}</span></p>
+                                    <a href="{{ route('quiz.show', $quiz->slug) }}"
+                                       class="inline-block bg-green-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-green-600">
+                                        {{ $quiz->title }}
+                                    </a>
+                                    <p class="text-sm mt-2">Questions: <span>{{ $quiz->questions_count }}</span></p>
                                 </div>
                             </div>
                         </div>
