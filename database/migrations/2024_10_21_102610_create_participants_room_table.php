511f5e4342d087967a2f26707901e8baa438209e
tinebasilio <kristinebasilio28@gmail.com>
2024-10-26 19:01:59 +0800

    Add database migrations

---
+++ database/migrations/2024_10_21_102610_create_participants_room_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participants_room', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id'); // Correct data type for foreign key
+            $table->unsignedBigInteger('participant_id'); // Correct data type for foreign key
+            $table->boolean('Is_at_room')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Add foreign key constraints
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->foreign('participant_id')->references('id')->on('participants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants_room');
+    }
+};
