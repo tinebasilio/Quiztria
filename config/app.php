b6843ae96b3aed2ae61b8730d9fe3d1ad6beda59
Paul Trustan Yumang <yumang.paultrustan@auf.edu.ph>
2024-11-07 22:31:21 +0800

    Add pdf function

--- config/app.php
+++ config/app.php
@@ -165,10 +165,12 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Livewire\LivewireServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
+
 
     ])->toArray(),
 
@@ -185,6 +187,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];
