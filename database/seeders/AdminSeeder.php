4cf5707b4bd2330b8beaa3397bf07a1d7601493b
Paul Trustan Yumang <yumang.paultrustan@auf.edu.ph>
2024-11-07 22:43:47 +0800

    Add Seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->admin()->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'quiztria@gmail.com',
+            'password' => Hash::make('QuiztriaAdmin'), // Secure this in production
+            'is_admin' => 1, // Assuming 1 is for admin based on your setup
+        ]);
     }
 }
+
